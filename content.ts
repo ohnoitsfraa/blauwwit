d4c97ba72d2386b9d8b02466aeaccc636a2bd899
Frankie Claessens <frankie.claessens@forward.eu>
2023-09-18 16:27:39 +0200

    Added feature to ignore topics

--- content.ts
+++ content.ts
@@ -1,5 +1,10 @@
 import type { PlasmoCSConfig } from "plasmo"
-import extConfig from './extConfig.json';
+import { Storage } from '@plasmohq/storage';
+
+import extConfig from "./extConfig.json"
+
+const parser = new DOMParser()
+const storage = new Storage()
 
 export const config: PlasmoCSConfig = {
     matches: ["https://www.blauwwit.be/*"]
@@ -12,20 +17,12 @@ function wrap(el, wrapper) {
     }
 }
 
-function unwrap(el) {
-    if (el && el.parentNode) {
-        // move all children out of the element
-        while (el.firstChild) {
-            el.parentNode.insertBefore(el.firstChild, el)
-        }
-        // remove the empty element
-        el.remove()
-    }
+function insertAfter(referenceNode, newNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling)
 }
 
 const hideFoes = async () => {
     const result = await (await fetch(extConfig.ucpUrl)).text()
-    const parser = new DOMParser()
     const htmlResult = parser.parseFromString(result, "text/html")
     htmlResult.documentElement
         .querySelectorAll('select[name="usernames[]"] option')
@@ -34,8 +31,8 @@ const hideFoes = async () => {
                 value: node.getAttribute("value"),
                 name: node.innerHTML
             }
-            console.log(`Found blauwwit quote by foe: ${foe.name}`);
             document.querySelectorAll(`blockquote cite a`).forEach((link) => {
+                console.log(`Found blauwwit quote by foe: ${foe.name}`)
                 if (link.textContent.indexOf(foe.name) < 0) {
                     return
                 }
@@ -44,7 +41,7 @@ const hideFoes = async () => {
                     `<div class="ignore-message">
                         <a href="./memberlist.php?mode=viewprofile&amp;u=${foe.value}" style="color: #888888;" class="username-coloured">${foe.name}</a>
                         <span>die momenteel op je negeerlijst staat, is geciteerd in dit bericht.</span>
-                        <a class="remove-ignore" style="cursor: pointer;">Dit bericht weergeven</a>
+                        <a class="remove-ignore post" style="cursor: pointer;">Dit bericht weergeven</a>
                     </div>
                     `,
                     "text/html"
@@ -54,28 +51,115 @@ const hideFoes = async () => {
                     `<div class="ignored" style="display:none !important;"></div>`,
                     "text/html"
                 )
-                wrap(div, wrapperElement.body.firstChild);
+                wrap(div, wrapperElement.body.firstChild)
             })
         })
+};
+
+const hideTopics = async () => {
+    let hiddenTopics: any[] = await storage.get('hiddenTopics') || [];
+    if (hiddenTopics) {
+        hiddenTopics.forEach(topic => {
+            const selector = `a.topictitle[href$="${topic.url}"]`;
+            document.querySelectorAll(selector).forEach(link => {
+                try {
+                    const dl = link.parentElement.parentElement.parentElement;
+                    dl.classList.add('hide');
+                    dl.style.paddingTop = '2px';
+                    dl.style.paddingBottom = '2px';
+                    dl.setAttribute('data-topic-value', topic.value);
+                    const exists = dl.parentElement.querySelectorAll('.ignore-message');
+                    if (!(exists && exists.length > 0)) {
+                        dl.parentElement.appendChild(
+                            parser.parseFromString(
+                                `<div class="ignore-message">
+                                    <span>Dit topic staat op je negeerlijst</span>
+                                    <a class="remove-ignore topic" style="cursor: pointer;">Dit topic verwijderen van je negeerlijst en weergeven</a>
+                                </div>`,
+                                'text/html').body.firstChild);
+                    }
+                } catch (e) {
+                    console.log('dl not found for: ', link);
+                }
+
+            });
+        });
+    }
+}
+
+const showHideTopicToggles = () => {
+    document.querySelectorAll('a.topictitle, .topic-title a').forEach((topicTitle) => {
+        const hideTopicAnchor = parser.parseFromString(
+            `<svg class="hide-topic" fill="#888888" height="10px" width="10px" style="cursor:pointer;margin-left:5px;text-decoration:none;" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 65.518 65.518" xml:space="preserve"><g><path d="M32.759,0C14.696,0,0,14.695,0,32.759s14.695,32.759,32.759,32.759s32.759-14.695,32.759-32.759S50.822,0,32.759,0z M6,32.759C6,18.004,18.004,6,32.759,6c6.648,0,12.734,2.443,17.419,6.472L12.472,50.178C8.443,45.493,6,39.407,6,32.759z M32.759,59.518c-5.948,0-11.447-1.953-15.895-5.248l37.405-37.405c3.295,4.448,5.248,9.947,5.248,15.895 C59.518,47.514,47.514,59.518,32.759,59.518z"/></g></svg>`,
+            "text/html"
+        ).body.firstChild
+        insertAfter(topicTitle, hideTopicAnchor)
+    })
+};
+
+const watchStorage = () => {
+    storage.watch({
+        'hiddenTopics': () => {
+            hideTopics();
+        }
+    });
 }
 
 window.addEventListener("load", () => {
     hideFoes()
-})
+    showHideTopicToggles();
+    hideTopics();
+    watchStorage();
+});
 
 document.addEventListener(
     "click",
-    function (event) {
+    async function (event) {
         const target = <HTMLElement>event.target
         if (target.matches(".remove-ignore")) {
             event.preventDefault()
-            const blockQuote = target.parentElement.parentElement;
-            const div = blockQuote.querySelector(".ignored > div");
-            const ignored = blockQuote.querySelector('.ignored');
-            blockQuote.prepend(div);
-            blockQuote.removeChild(ignored);
-            const parent = target.parentElement
-            parent.parentElement.removeChild(parent)
+            if (target.matches('.topic')) {
+                const dl = target.parentElement.parentElement.querySelector('dl');
+                dl.classList.remove('hide');
+                const ignored = dl.parentElement.querySelector('.ignore-message');
+                dl.parentElement.removeChild(ignored);
+                let hiddenTopics: any[] = await storage.get('hiddenTopics') || [];
+                const topicValue = parseInt(dl.getAttribute('data-topic-value'));
+                const index = hiddenTopics.findIndex(hiddenTopic => hiddenTopic.value === topicValue);
+                hiddenTopics.splice(index, 1);
+                await storage.set('hiddenTopics', hiddenTopics);
+            } else if (target.matches('.post')) {
+                const blockQuote = target.parentElement.parentElement;
+                const div = blockQuote.querySelector(".ignored > div")
+                const ignored = blockQuote.querySelector(".ignored")
+                blockQuote.prepend(div)
+                blockQuote.removeChild(ignored)
+                const parent = target.parentElement
+                parent.parentElement.removeChild(parent)
+            }
+        } else if (target.matches(".hide-topic")) {
+            event.preventDefault()
+            const anchor = <HTMLElement>target.previousSibling
+            if (anchor) {
+                let href = anchor.getAttribute("href");
+                if (href.charAt(0) === '.') {
+                    href = href.slice(1);
+                }
+                const url = new URL(location.origin + href);
+                const urlParams = new URLSearchParams(url.search);
+                const topicId = urlParams.get('t');
+                let hiddenTopics: any[] = await storage.get('hiddenTopics') || [];
+                const exists = hiddenTopics.find(topic => topic.value === topic);
+                if (!exists) {
+                    hiddenTopics.push({
+                        value: topicId,
+                        title: anchor.innerText,
+                        url: anchor.getAttribute("href"),
+                        absUrl: url
+                    });
+                }
+                storage.set('hiddenTopics', hiddenTopics);
+            }
         }
     },
     false
--- popup.tsx
+++ popup.tsx
@@ -1,8 +1,11 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
+import { Storage } from '@plasmohq/storage';
 import extConfig from './extConfig.json';
 
 function IndexPopup() {
-  const [data, setData] = useState([])
+  const [foes, setFoes] = useState([]);
+  const [hiddenTopics, setHiddenTopics] = useState([]);
+  const storage = new Storage()
 
   async function getFoes(): Promise<void> {
     const result = await (await fetch(extConfig.ucpUrl)).text()
@@ -18,15 +21,41 @@ function IndexPopup() {
         }
         foes.push(foe);
       });
-    setData(foes);
+    setFoes(foes);
   }
 
-  function handleClick(): void {
-    window.open(extConfig.ucpUrl, '_blank');
+  async function getHiddenTopics(): Promise<void> {
+    setHiddenTopics(await storage.get('hiddenTopics'));
+    console.log(hiddenTopics);
+  }
+
+  function goToHandleClick(url): void {
+    window.open(url, '_blank');
+  }
+
+  async function deleteHandleClick(topic): Promise<void> {
+    if (typeof topic === 'undefined') {
+      await storage.set('hiddenTopics', [])
+      setHiddenTopics([]);
+    } else {
+      const index = hiddenTopics.findIndex(hiddenTopic => hiddenTopic.value === topic.value);
+      hiddenTopics.splice(index, 1);
+      await storage.set('hiddenTopics', hiddenTopics);
+    }
+
+    getHiddenTopics();
+  }
+
+  function watchStorage(): void {
+    storage.watch({
+      'hiddenTopics': () => getHiddenTopics()
+    });
   }
 
   useEffect(() => {
     getFoes();
+    getHiddenTopics();
+    watchStorage();
   }, [])
 
   return (
@@ -36,7 +65,7 @@ function IndexPopup() {
         fontFamily: 'Verdana sans-serif',
         flexDirection: "column",
         padding: 16,
-        minWidth: 200,
+        minWidth: 320,
         background: "radial-gradient(ellipse at center, rgba(37, 99, 151, 1) 0%, rgba(37, 99, 151, 1) 15%, rgba(8, 49, 75, 1) 100%)",
         color: '#FFF',
         margin: 0
@@ -55,21 +84,23 @@ function IndexPopup() {
         }}>wit</span><span style={{
           fontSize: "0.5em"
         }}>.be</span></h1>
-      {data && data.length > 0 ? (
+      <p style={{
+          fontFamily: "Verdana, Helvetica, Arial, sans-serif",
+          fontSize: 14,
+          marginTop: 10,
+          marginBottom: 0,
+          color: '#FFF',
+          opacity: '0.3',
+          letterSpacing: 2
+        }}>GENEGEERDE GEBRUIKERS {foes ? `(${foes.length})` : ''}</p>
+      {foes && foes.length > 0 ? (
         <div>
-          <p style={{
-            fontFamily: "Verdana, Helvetica, Arial, sans-serif",
-            fontSize: 20,
-            marginTop: 10,
-            marginBottom: 0,
-            color: '#FFF',
-            opacity: '0.3',
-            letterSpacing: 4
-          }}>NEGEERLIJST</p>
           <ul id="foelist" style={{
-            paddingLeft: 5
+            paddingLeft: 0,
+            maxHeight: 150,
+            overflowY: 'auto'
           }}>
-            {data.map(foe => {
+            {foes.map(foe => {
               return <li style={{
                 fontFamily: "Verdana, Helvetica, Arial, sans-serif",
                 fontSize: 12,
@@ -82,15 +113,94 @@ function IndexPopup() {
         </div>) : null
       }
       <button style={{
-        border: "1px solid #31567B",
+        fontFamily: "Verdana, Helvetica, Arial, sans-serif",
         borderRadius: 3,
-        background: "#011623",
+        background: 'transparent',
+        border: 'none',
+        textDecoration: 'underline',
         color: "#DDDDDD",
         cursor: "pointer",
-        padding: "10px 8px",
-        fontSize: "1.1em",
-        marginTop: 10
-      }} id="goto" onClick={() => handleClick()}>Ga naar negeerlijst</button>
+        padding: 0,
+        textAlign: 'left',
+        fontSize: 12,
+        marginTop: 10,
+        marginBottom: 10
+      }} id="goto" onClick={() => goToHandleClick(extConfig.ucpUrl)}>Ga naar negeerlijst</button>
+      <hr style={{
+        width: '100%',
+        opacity: 0.3,
+        marginTop: 10,
+        marginBottom: 10
+        }}/>
+      <p style={{
+          fontFamily: "Verdana, Helvetica, Arial, sans-serif",
+          fontSize: 14,
+          marginTop: 10,
+          marginBottom: 0,
+          color: '#FFF',
+          opacity: '0.3',
+          letterSpacing: 2
+        }}>GENEGEERDE TOPICS {hiddenTopics ? `(${hiddenTopics.length})` : ''}</p>
+      {hiddenTopics && hiddenTopics.length > 0 ? (
+        <div>
+          <ul id="topiclist" style={{
+            paddingLeft: 0,
+            maxHeight: 150,
+            overflowY: 'auto'
+          }}>
+            {hiddenTopics.map(topic => {
+              return <li style={{
+                fontFamily: "Verdana, Helvetica, Arial, sans-serif",
+                fontSize: 12,
+                listStyle: 'none',
+                marginTop: 5,
+                marginBottom: 5,
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'space-between'
+              }} data-value={topic.value} key={topic.value}>
+                <a onClick={() => goToHandleClick(topic.absUrl)} style={{
+                  whiteSpace: 'nowrap',
+                  overflow: 'hidden',
+                  flexBasis: 'calc(100% - 30px)',
+                  textDecoration: 'underline',
+                  cursor: 'pointer',
+                  textOverflow: 'ellipsis'
+                }}>{topic.title}</a>
+                <button style={{
+                  border: "1px solid #31567B",
+                  borderRadius: 3,
+                  background: "#011623",
+                  color: "#DDDDDD",
+                  cursor: "pointer",
+                  fontSize: "1em",
+                  marginTop: 10,
+                  marginBottom: 10,
+                  padding: 0,
+                  margin: 0,
+                  width: 20,
+                  height: 20,
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center'
+                }} id="deleteTopics" onClick={() => deleteHandleClick(topic)}>x</button>
+              </li>
+            })}
+          </ul>
+          <button style={{
+            width: '100%',
+            border: "1px solid #31567B",
+            borderRadius: 3,
+            background: "#011623",
+            color: "#DDDDDD",
+            cursor: "pointer",
+            padding: "10px 8px",
+            fontSize: "1.1em",
+            marginTop: 10,
+            marginBottom: 10
+          }} id="deleteTopics" onClick={() => deleteHandleClick(undefined)}>Verwijder alle topics</button>
+        </div>) : null
+      }
     </div>
   )
 }
